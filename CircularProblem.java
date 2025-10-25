b2b2170b62e4d7677eeedb86416c6e7e87fb159a
Raymond Lin <raymondl183@nycstudents.net>
2025-10-25 16:45:38 +0000

    eighth commit

--- CircularProblem.java
+++ CircularProblem.java
@@ -1,9 +1,12 @@
+import java.util.Scanner;
 public class CircularProblem {
 public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    System.out.print("Enter number: ");
+    int n = scan.nextInt();
     int ans = 0;
     int i = 0;
     int series = 0;
-    int n = 2025;
     while (series < n) {
         if (n <= (series + Math.pow(2, i))) {
             break;
@@ -19,5 +22,6 @@ public static void main(String[] args) {
         ans = 1 + ((pos - 1) * 2);
     }
     System.out.println(ans);
+    scan.close();
 }
 }
